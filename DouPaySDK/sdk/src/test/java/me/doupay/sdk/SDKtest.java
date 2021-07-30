94dee4ab8b18302fec0272a075719bc3e6a6d9cb
admin <test@test.com>
2021-07-30 16:24:25 +0800

    添加项目

---
+++ DouPaySDK/sdk/src/test/java/me/doupay/sdk/SDKtest.java
@@ -0,0 +1,226 @@
+package me.doupay.sdk;
+
+import me.doupay.sdk.bean.*;
+import me.doupay.sdk.enums.RefundType;
+import me.doupay.sdk.interfaceCallback.CallBackListener;
+import me.doupay.sdk.net.BaseVo;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+
+public class SDKtest {
+
+
+    public  static  void  initAllParameters() {
+        String appId = "xxxx";
+        String secret = "xxxx";
+        String privateKey = "xxx";
+        String publicKey = "xxxx";
+        Constants.openSysLog = true;
+        Constants.getInstance().init(secret,privateKey,publicKey,appId,"86400");
+    }
+
+    @Test
+    public void getCoinList () {
+        initAllParameters();
+
+        BaseVo<CoinResponseData> baseVo = PaymentInfo.getCoinList();
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+
+
+    }
+    @Test
+    public void getCurrencyList() {
+        initAllParameters();
+
+        BaseVo<CurrencyResponseData> baseVo = PaymentInfo.getCurrencyList();
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void getPay() {
+        initAllParameters();
+        String orderNo = "SJDD" + String.valueOf(System.currentTimeMillis());
+
+        BaseVo<PayResponseData> baseVo = PaymentInfo.pay("","12", CoinNameEnum.USDT, CurrencyCodeEnum.CNY, "17788888888", orderNo,
+                "我很好啊啊", "", "", OrderTypeCodeEnum.BY_AMOUNT);
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void cancle() {
+        initAllParameters();
+
+        BaseVo<PayResponseData> baseVo =  PaymentInfo.cancleOrder("ZF202107261159264168428799");
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void getOrderInfo() {
+        initAllParameters();
+        BaseVo<OrderInfoResponseData> baseVo = PaymentInfo.getOrderInfo("ZF202107291547355277965323");
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void getPaymentInfo() {
+        initAllParameters();
+
+        BaseVo<PaymentInfoResponseData> baseVo =  PaymentInfo.getPaymentInfo(CoinNameEnum.USDT,"0004" ,"ZF202107280739365664124421");
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void getRefund() {
+        initAllParameters();
+        BaseVo<RefundResponseData> baseVo = PaymentInfo.refund(RefundType.NEW_ADDRESS,"TEQrvHyU54YibVHMGb7475n8y3mXBofaaR", "5.8", "ZF202107280852374857730103", "退1个,啦啦啦");
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void getRefundInfo() {
+        initAllParameters();
+
+        BaseVo<RefundInfoResponseData> baseVo = PaymentInfo.getRefunds("ZF202107270223484993585691");
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void withdraw() {
+        initAllParameters();
+        String orderNo = "SHYH" + String.valueOf(System.currentTimeMillis());
+        BaseVo<WithdrawResponse> baseVo = PaymentInfo.withdraw("TEQrvHyU54YibVHMGb7475n8y3mXBofaaR", "5", CoinNameEnum.USDT, orderNo, orderNo,"500",OrderTypeCodeEnum.BY_MONEY,CurrencyCodeEnum.CNY);
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void makeup() {
+        initAllParameters();
+        BaseVo<MakeUpResponse> baseVo = PaymentInfo.maleUp("需要补单","ZF202107281302375977458662");
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void getCallBack() {
+        initAllParameters();
+        BaseVo<PaymentCallBackResponse> baseVo = PaymentInfo.getCallback("ZF202107221114581046391184");
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void getPrice() {
+        initAllParameters();
+        BaseVo<CoinPriceResponse> baseVo = PaymentInfo.getCoinPrice("100","100",OrderTypeCodeEnum.BY_MONEY,CoinNameEnum.USDT,CurrencyCodeEnum.CNY);
+        if (baseVo.getCode() == 200) {
+            System.out.println("-------------------------" + baseVo.getData().toString());
+        }else {
+            System.out.println(baseVo.getCode() + "-------------------------" + baseVo.getMsg());
+        }
+    }
+
+    @Test
+    public void verifySignAndGetResult () {
+        initAllParameters();
+
+        String bodystring = "{\"address\":\"TQ1EgPhuDXLvDfycCBQadbfbLkBPhEDoZX\",\"amountPaid\":\"15.26717557\",\"coinName\":\"USDT\",\"orderCode\":\"ZF202106221133335640422688\",\"paymentStatus\":1,\"protocolName\":\"TRC20\",\"result\":true}";
+        String headstring = "uCJasnGz3H+2Xi86zeRkoDVEaXpM0LsCInOW1Kk28pn/3bXMn/2vUac7aQrWUzcf8YjTiXJt03IzS6k0y04TsvL1rdOj7TZgrk2aav8wM79ECviDW1+OQWd2XRhCRJGL4ca5lnXiE+1eHV5Tdq68Nu50tOkTKGI6cD3LT6imEWq5bTB0I+xuabnhSDfhWdmogmzEwKju3xSPcjxtzHGJ/c8Zc1NYH7ID92qWi77Wm6/UskHiATgULXYCbLHL/hBFDn4kGH8E+FQ0oSoPOTLOvpKnI27v6xD7CyHhMUOYRaVm5u/X4YNta65hF//JVPLnMw8I9NJA9WklDPFZ1DvIQQ==";
+        PaymentInfo.verifySignAndGetResult(headstring, bodystring, new CallBackListener<PaymentCallBackResponse>() {
+            @Override
+            public void onFinish(PaymentCallBackResponse data) {
+                System.out.println("PaymentResultResponse = " + data.toString());
+            }
+
+            @Override
+            public void onError(int errorCode, String msg) {
+                System.out.println("errorCode=" + errorCode + "msg =" + msg);
+
+            }
+        }, new CallBackListener<UserWithdrawCallBackResponse>() {
+            @Override
+            public void onFinish(UserWithdrawCallBackResponse data) {
+                System.out.println("UserWithdrawCallBackResponse = " + data.toString());
+            }
+
+            @Override
+            public void onError(int errorCode, String msg) {
+                System.out.println("errorCode=" + errorCode + "msg =" + msg);
+            }
+        }, new CallBackListener<MakeUpCallBackResponse>() {
+            @Override
+            public void onFinish(MakeUpCallBackResponse data) {
+                System.out.println("UserWithdrawCallBackResponse = " + data.toString());
+            }
+
+            @Override
+            public void onError(int errorCode, String msg) {
+                System.out.println("errorCode=" + errorCode + "msg =" + msg);
+            }
+        });
+    }
+
+    @Test
+    public void  getBillRecord () {
+        initAllParameters();
+        LocalDateTime start =  LocalDateTime.now();
+        LocalDateTime end = LocalDateTime.now();
+//        PaymentInfo.getBillRecords( null, null, 10, 1, new CallBackListener<BillRecord>() {
+//            @Override
+//            public void onFinish(BillRecord data) {
+//
+//            }
+//
+//            @Override
+//            public void onError(int errorCode, String msg) {
+//
+//            }
+//        });
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# doupay-java
+# 该项目支持两种集成方式,现在暂时支持第一种
+
+## 1.jar包引入
+   ### 1.1 把jar包拖入项目,如下图所示:jar包在文件中即DouPaySdk.jar,也可以自己用源代码打包
+   
+   <img width="1488" alt="1111" src="https://user-images.githubusercontent.com/86946898/127596834-639f94c0-e775-4911-9433-f976589852ee.png">
+   
+  ### 1.2 pom.xml配置文件中添加以下依赖,代码和截图见下方
+  
+   ```javascript
+        <dependency>
+            <groupId>com.github.doupay</groupId>
+            <artifactId>doupay-java</artifactId>
+            <version>1.0.8</version>
+            <scope>system</scope>
+            <systemPath>${basedir}/src/main/resources/lib/DouPaySDK.jar</systemPath>
+        </dependency>
+   ```
+   
+<img width="1644" alt="222" src="https://user-images.githubusercontent.com/86946898/127596733-766e8f04-8cd6-4d86-bc1a-d52e5c6e7213.png">
+
+### 项目中调用初始化方法,示例如下:
+
+![1111](https://user-images.githubusercontent.com/86946898/126156599-e3527fa4-4848-423a-b344-b383fc67fffb.png)
+
+#### secret为商户管理后台创建app得到的,appid为创建app得到的,privateKey为自己的私钥,publicKey为交换公钥后得到的平台公钥,expireTime为订单过期时间,秒为单位
+
+## sdk中各方法请求和响应参数,请参照[wiki](https://github.com/doupay/doupay-java/wiki)
+
+## 2.maven,使用方法如下:
+
+### 1.将 JitPack存储库添加到您的构建文件pom.xml
+
+```javascript
+ <repositories>
+	<repository>
+	     <id>jitpack.io</id>
+		   <url>https://jitpack.io</url>
+	</repository>
+ </repositories>
+```
+
+###  2.添加依赖项
+
+```javascript
+  <dependency>
+	  <groupId>com.github.doupay</groupId>
+	  <artifactId>doupay-java</artifactId>
+	  <version>0.0.1</version>
+   </dependency>
+```
+#### 示例截图如下:
+图中示例版本号"0.0.1"会随时变化,请随时关注本github更新
+
+<img width="864" alt="步骤" src="https://user-images.githubusercontent.com/86946898/124468680-feed5b80-ddcb-11eb-927f-c10855eeaf86.png">
+
+### 项目中调用初始化方法,示例如下:
+
+![1111](https://user-images.githubusercontent.com/86946898/126156599-e3527fa4-4848-423a-b344-b383fc67fffb.png)
+
+#### secret为商户管理后台创建app得到的,appid为创建app得到的,privateKey为自己的私钥,publicKey为交换公钥后得到的平台公钥,expireTime为订单过期时间,秒为单位
+
+## sdk中各方法请求和响应参数,请参照[wiki](https://github.com/doupay/doupay-java/wiki)
