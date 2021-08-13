745eb286180141ab08da56de54d314d84b4f39ae
admin <test@test.com>
2021-08-13 10:29:21 +0800

    修改查询链币种信息的实体类

--- DouPaySDK/sdk/src/main/java/me/doupay/sdk/bean/CoinChainCodeResp.java
+++ DouPaySDK/sdk/src/main/java/me/doupay/sdk/bean/CoinChainCodeResp.java
@@ -1,55 +1,80 @@
 package me.doupay.sdk.bean;
 
+import java.util.List;
+
 public class CoinChainCodeResp {
 
-    /// 链币种代码
-    private String chainCoinCode;
-    /// 链币种名称
-    private String chainCoinName;
-    /// 协议代码
-    private String protocolCode;
-    /// 协议名称
-    private String protocolName;
 
-    public String getChainCoinCode() {
-        return chainCoinCode;
+    private List<RecordsBean> records;
+
+    public List<RecordsBean> getRecords() {
+        return records;
     }
 
-    public void setChainCoinCode(String chainCoinCode) {
-        this.chainCoinCode = chainCoinCode;
+    public void setRecords(List<RecordsBean> records) {
+        this.records = records;
     }
 
-    public String getChainCoinName() {
-        return chainCoinName;
-    }
+    public static class RecordsBean {
+        /**
+         * protocolCode : 0006
+         * protocolName : TRC20
+         * chainCoinName : USDT
+         * chainCoinCode : 0006
+         */
 
-    public void setChainCoinName(String chainCoinName) {
-        this.chainCoinName = chainCoinName;
-    }
+        private String protocolCode;
+        private String protocolName;
+        private String chainCoinName;
+        private String chainCoinCode;
 
-    public String getProtocolCode() {
-        return protocolCode;
-    }
+        public String getProtocolCode() {
+            return protocolCode;
+        }
 
-    public void setProtocolCode(String protocolCode) {
-        this.protocolCode = protocolCode;
-    }
+        public void setProtocolCode(String protocolCode) {
+            this.protocolCode = protocolCode;
+        }
 
-    public String getProtocolName() {
-        return protocolName;
-    }
+        public String getProtocolName() {
+            return protocolName;
+        }
 
-    public void setProtocolName(String protocolName) {
-        this.protocolName = protocolName;
+        public void setProtocolName(String protocolName) {
+            this.protocolName = protocolName;
+        }
+
+        public String getChainCoinName() {
+            return chainCoinName;
+        }
+
+        public void setChainCoinName(String chainCoinName) {
+            this.chainCoinName = chainCoinName;
+        }
+
+        public String getChainCoinCode() {
+            return chainCoinCode;
+        }
+
+        public void setChainCoinCode(String chainCoinCode) {
+            this.chainCoinCode = chainCoinCode;
+        }
+
+        @Override
+        public String toString() {
+            return "RecordsBean{" +
+                    "protocolCode='" + protocolCode + '\'' +
+                    ", protocolName='" + protocolName + '\'' +
+                    ", chainCoinName='" + chainCoinName + '\'' +
+                    ", chainCoinCode='" + chainCoinCode + '\'' +
+                    '}';
+        }
     }
 
     @Override
     public String toString() {
         return "CoinChainCodeResp{" +
-                "chainCoinCode='" + chainCoinCode + '\'' +
-                ", chainCoinName='" + chainCoinName + '\'' +
-                ", protocolCode='" + protocolCode + '\'' +
-                ", protocolName='" + protocolName + '\'' +
+                "records=" + records +
                 '}';
     }
 }
